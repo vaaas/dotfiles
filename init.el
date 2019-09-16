821069bb4cd97a9c3b5257e486ce87e73518d653
Vasileios Pasialiokis <whiterocket@outlook.com>
2019-09-16 18:44:36 +0300

    emacs is back

---
+++ init.el
@@ -0,0 +1,246 @@
+; function definitions
+(defun insert-tab-or-indent(beg end) (interactive "r")
+	(if (region-active-p)
+		(indent-rigidly-right-to-tab-stop beg end)
+		(insert-tab)))
+
+(defun insert-tab() (interactive) (insert-char 09))
+
+(defun newline-and-indent-relative() (interactive)
+	(newline)
+	(indent-relative-first-indent-point))
+
+(defun sxiv-ref() (interactive)
+	(start-process "sxiv" "sxiv" "sxiv" "ref/"))
+
+(defun split-newlines(string)
+	(split-string string "\n"))
+
+(defun filedb-open() (interactive)
+	(find-file
+	(ido-completing-read "select a file > "
+	(split-newlines
+	(shell-command-to-string "xzcat ~/filedb.xz")))))
+
+(defun newline-above() (interactive)
+	(beginning-of-line)
+	(newline)
+	(previous-line))
+
+(defun newline-below() (interactive)
+	(end-of-line)
+	(newline))
+
+(defun double-newline() (interactive)
+	(newline) (newline))
+
+(defun vi-open-above() (interactive)
+	(newline-above)
+	(vi-mode -1))
+
+(defun vi-open-below() (interactive)
+	(newline-below)
+	(vi-mode -1))
+
+(defun vi-change-line() (interactive)
+	(kill-whole-line)
+	(vi-mode -1))
+
+(defun vi-change-next-word() (interactive)
+	(kill-word)
+	(vi-mode -1))
+
+(defun vi-change-previous-word() (interactive)
+	(backward-kill-word)
+	(vi-mode -1))
+
+(defun yank-this-line() (interactive)
+	(kill-ring-save (line-beginning-position) (line-end-position)))
+
+(defun paste-below() (interactive)
+	(newline-below)
+	(yank))
+
+(defun paste-above() (interactive)
+	(newline-above)
+	(yank))
+
+(custom-set-variables
+	'(package-selected-packages '(company markdown-mode)))
+(custom-set-faces
+	'(error ((t (:foreground "#dc322f" :weight bold))))
+	'(font-lock-comment-face ((t (:foreground "#859900"))))
+	'(font-lock-constant-face ((t (:foreground "#6c71c4"))))
+	'(font-lock-function-name-face ((t (:foreground "#268bd2"))))
+	'(font-lock-keyword-face ((t (:foreground "#d33682"))))
+	'(font-lock-string-face ((t (:foreground "#cb4b16"))))
+	'(link ((t (:foreground "#2aa198" :underline t))))
+	'(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :height 1.5))))
+	'(shadow ((t (:foreground "#93a1a1")))))
+
+(require 'package)
+(add-to-list 'package-archives
+	'("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(add-to-list 'write-file-functions 'delete-trailing-whitespace)
+
+; fonts, colors
+(setq default-frame-alist '(
+	(font . "Monospace 16")
+	(background-color . "#fdf6e3")
+	(foreground-color . "#073642")))
+
+(add-hook 'window-setup-hook (lambda()
+	(scroll-bar-mode -1)
+	(tool-bar-mode -1)
+	(menu-bar-mode -1)
+	(set-fringe-mode 0)))
+
+; keys
+(define-key global-map (kbd "C-\\") nil)
+
+(define-key global-map (kbd "C-s") 'save-buffer)
+(define-key global-map (kbd "C-w") 'backward-kill-word)
+(define-key global-map (kbd "C-S-P") 'execute-extended-command)
+(define-key global-map (kbd "C-z") 'undo)
+(define-key global-map (kbd "C-v") 'yank)
+(define-key global-map (kbd "C-x") 'kill-region)
+(define-key global-map (kbd "C-S-X") 'kill-ring-save)
+(define-key global-map (kbd "C-i") 'dabbrev-expand)
+(define-key global-map (kbd "C-o") 'find-file)
+(define-key global-map (kbd "C-S-O") 'filedb-open)
+(define-key global-map (kbd "<escape>") 'vi-mode)
+(define-key global-map (kbd "C-f") 'isearch-forward)
+(define-key global-map (kbd "C-S-F") 'isearch-backward)
+(define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "C-S-F") 'isearch-repeat-forward)
+(define-key global-map (kbd "C-r") 'replace-string)
+(define-key global-map (kbd "C-S-R") 'replace-regexp)
+(define-key minibuffer-local-map (kbd "<escape>") 'abort-recursive-edit)
+(define-key global-map (kbd "M-o") 'other-window)
+(define-key global-map (kbd "M-C-O") 'delete-other-windows)
+(define-key global-map (kbd "M-[") 'previous-buffer)
+(define-key global-map (kbd "M-]") 'next-buffer)
+(define-key global-map (kbd "C-<tab>") 'switch-to-buffer)
+(define-key global-map (kbd "M-<tab>") 'mode-line-other-buffer)
+(define-key global-map (kbd "M-a") 'set-mark-command)
+(define-key global-map (kbd "C-k") 'kill-whole-line)
+(define-key global-map (kbd "C-q") 'kill-this-buffer)
+(define-key global-map (kbd "C-d") 'kill-word)
+(define-key global-map (kbd "C-`") 'eshell)
+(define-key global-map (kbd "C-j") 'newline-below)
+(define-key global-map (kbd "C-S-J") 'newline-above)
+(define-key global-map (kbd "M-q") 'kmacro-start-macro)
+(define-key global-map (kbd "M-Q") 'kmacro-end-or-call-macro)
+
+(define-key global-map (kbd "C-\\ e b") 'eval-buffer)
+(define-key global-map (kbd "C-\\ r e f") 'sxiv-ref)
+(define-key global-map (kbd "C-\\ w c") 'count-words-region)
+(define-key global-map (kbd "C-\\ a") 'mark-whole-buffer)
+(define-key global-map (kbd "C-\\ v p") 'variable-pitch-mode)
+(define-key global-map (kbd "C-\\ f") 'text-scale-adjust)
+
+(define-key prog-mode-map (kbd "<tab>") 'insert-tab-or-indent)
+(define-key text-mode-map (kbd "<tab>") 'insert-tab-or-indent)
+(define-key emacs-lisp-mode-map (kbd "<tab>") 'insert-tab-or-indent)
+(define-key global-map (kbd "<backtab>") 'indent-rigidly-left-to-tab-stop)
+
+(define-key prog-mode-map (kbd "<return>") 'newline-and-indent-relative)
+(define-key text-mode-map (kbd "<C-return>") 'newline-and-indent-relative)
+(define-key emacs-lisp-mode-map (kbd "<return>") 'newline-and-indent-relative)
+
+(define-key text-mode-map (kbd "<backspace>") 'backward-delete-char)
+(define-key prog-mode-map (kbd "<backspace>") 'backward-delete-char)
+(define-key emacs-lisp-mode-map (kbd "<backspace>") 'backward-delete-char)
+
+(define-key text-mode-map (kbd "<return>") 'double-newline)
+
+(with-eval-after-load 'company
+	(define-key company-active-map (kbd "M-n") nil)
+	(define-key company-active-map (kbd "M-p") nil)
+	(define-key company-active-map (kbd "C-j") 'company-select-next)
+	(define-key company-active-map (kbd "C-k") 'company-select-previous)
+	(define-key company-active-map (kbd "<tab>") 'company-complete)
+	(define-key company-active-map (kbd "<escape>") 'company-abort))
+
+(with-eval-after-load 'markdown-mode
+	(define-key markdown-mode-map	 (kbd "C-x") nil))
+
+(add-hook 'ido-setup-hook (lambda()
+	(define-key ido-completion-map (kbd "C-j") 'ido-next-match)
+	(define-key ido-completion-map (kbd "C-k") 'ido-prev-match)))
+
+; variables
+(setq-default mode-line-format nil)
+(setq-default left-margin-width 1 right-margin-width 1)
+(setq-default tab-width 3)
+(setq inhibit-splash-screen t)
+(setq make-backup-files nil)
+(setq frame-resize-pixelwise t)
+(setq-default lsp-enable-snippet nil)
+(setq company-idle-delay 0.1)
+(setq vc-follow-symlinks t)
+(setq-default cursor-type 'bar)
+
+; default modes
+(setq-default shift-select-mode nil)
+(setq-default indent-tabs-mode t)
+(ido-mode t)
+(electric-indent-mode -1)
+(global-visual-line-mode t)
+(line-number-mode -1)
+(set-window-margins (selected-window) 1 1)
+
+; hooks
+(add-hook 'js-mode-hook 'company-mode)
+(add-hook 'emacs-lisp-mode-hook 'company-mode)
+(add-hook 'markdown-mode-hook (lambda()
+	(variable-pitch-mode)
+	(text-scale-increase 2)))
+(add-hook 'python-mode-hook (lambda()
+	(setq indent-tabs-mode t
+		tab-width 3
+		py-indent-tabs-mode t)))
+
+; vi mode
+(setq vi-mode-map (make-sparse-keymap))
+(define-key vi-mode-map (kbd "i") 'vi-mode)
+(define-key vi-mode-map (kbd "h") 'left-char)
+(define-key vi-mode-map (kbd "l") 'right-char)
+(define-key vi-mode-map (kbd "k") 'previous-line)
+(define-key vi-mode-map (kbd "j") 'next-line)
+(define-key vi-mode-map (kbd "o") 'newline)
+(define-key vi-mode-map (kbd "O") 'newline-above)
+(define-key vi-mode-map (kbd "w") 'right-word)
+(define-key vi-mode-map (kbd "b") 'left-word)
+(define-key vi-mode-map (kbd "}") 'forward-paragraph)
+(define-key vi-mode-map (kbd "{") 'backward-paragraph)
+(define-key vi-mode-map (kbd "J") 'forward-paragraph)
+(define-key vi-mode-map (kbd "K") 'backward-paragraph)
+(define-key vi-mode-map (kbd "H") 'beginning-of-line-text)
+(define-key vi-mode-map (kbd "L") 'end-of-line)
+(define-key vi-mode-map (kbd "G") 'goto-line)
+(define-key vi-mode-map (kbd "v") 'set-mark-command)
+(define-key vi-mode-map (kbd "D") 'kill-ring)
+(define-key vi-mode-map (kbd "Y") 'kill-ring-save)
+(define-key vi-mode-map (kbd "d w") 'kill-word)
+(define-key vi-mode-map (kbd "d b") 'backward-kill-word)
+(define-key vi-mode-map (kbd "d d") 'kill-whole-line)
+(define-key vi-mode-map (kbd ".") 'repeat)
+(define-key vi-mode-map (kbd "u") 'undo)
+(define-key vi-mode-map (kbd "/") 'isearch-forward)
+(define-key vi-mode-map (kbd "p") 'paste-below)
+(define-key vi-mode-map (kbd "P") 'paste-above)
+(define-key vi-mode-map (kbd "SPC") 'execute-extended-command)
+(define-key vi-mode-map (kbd "o") 'vi-open-below)
+(define-key vi-mode-map (kbd "O") 'vi-open-above)
+(define-key vi-mode-map (kbd "q") 'kmacro-start-macro)
+(define-key vi-mode-map (kbd "Q") 'kmacro-end-or-call-macro)
+(define-key vi-mode-map (kbd "c c") 'vi-change-line)
+(define-key vi-mode-map (kbd "c w") 'vi-change-next-word)
+(define-key vi-mode-map (kbd "c w") 'vi-change-previous-word)
+(define-key vi-mode-map (kbd "y y") 'yank-this-line)
+(define-minor-mode vi-mode
+	"vi-like key bindings without modifier keys"
+	:keymap vi-mode-map)
