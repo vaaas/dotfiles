ee4fcface7cb90599cd6771b996a73aa34d2d86f
Vasileios Pasialiokis <whiterocket@outlook.com>
2019-09-27 00:27:18 +0300

    stuff

--- init.el
+++ init.el
@@ -92,6 +92,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "#d33682"))))
  '(error ((t (:foreground "#dc322f" :weight bold))))
  '(font-lock-comment-face ((t (:foreground "#859900"))))
  '(font-lock-constant-face ((t (:foreground "#6c71c4"))))
