5ddc79ad62e34b249300c8df569b0bed75fb3373
Vasileios Pasialiokis <whiterocket@outlook.com>
2019-09-28 20:34:39 +0300

    stuff

--- link.sh
+++ link.sh
@@ -6,5 +6,5 @@ do
 	src=$(echo $line | cut -f 1 -d '	')
 	dst=$(echo $line | cut -f 2 -d '	')
 	dst=${dst/#\~/$HOME}
-	ln -s -f -v -- $(realpath $src) $dst
+	ln -s -v -- $(realpath $src) $dst
 done < files.lst
