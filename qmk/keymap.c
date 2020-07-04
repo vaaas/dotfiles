8bbe92b322b8126087cbdb90ae6b64cc0a10ddcd
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-07-04 13:38:49 +0300

    stuff

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -22,17 +22,17 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,  \
+  KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_EQL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
   MO(1),  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_SPC, KC_RALT, KC_ENTER, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_PSCREEN, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLU, KC_VOLD,     KC_MUTE,     KC_7,    KC_8,      KC_9,    KC_DEL, \
+  KC_CAPS,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD, KC_VOLU,     KC_MUTE,     KC_7,    KC_8,      KC_9,    KC_DEL,    \
   KC_INSERT,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_GRV,  KC_LBRACKET, KC_RBRACKET, KC_4,    KC_5,      KC_6,    KC_BSLASH, \
-  KC_CAPS,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  KC_F14,      KC_F15,      KC_1,    KC_2,      KC_3,    KC_0, \
-  _______,    _______, _______, _______, _______, _______, _______,     _______,     KC_HOME, KC_PGDOWN, KC_PGUP, KC_END \
+  KC_PSCREEN, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F13,  KC_F14,      KC_F15,      KC_1,    KC_2,      KC_3,    KC_0,      \
+  _______,    _______, _______, _______, _______, _______, _______,     _______,     KC_HOME, KC_PGDOWN, KC_PGUP, KC_END     \
 ),  
 
 };
