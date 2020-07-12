55f78ee91da2f2db4935d66bf364cc0ba22f9de7
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-07-12 20:55:44 +0300

    stuff

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_CAPS,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_VOLD,  KC_VOLU,     KC_MUTE,     KC_7,    KC_8,      KC_9,    KC_DEL,    \
   KC_INSERT,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_GRV,   KC_LBRACKET, KC_RBRACKET, KC_4,    KC_5,      KC_6,    KC_BSLASH, \
-  KC_PSCREEN, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BRID,  KC_BRIU,     KC_MYCM,     KC_1,    KC_2,      KC_3,    KC_0,      \
-  _______,    _______, _______, _______, _______, KC_APP,   _______,     _______,     KC_HOME, KC_PGDOWN, KC_PGUP, KC_END     \
+  KC_PSCREEN, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BRID,  KC_BRIU,     KC_APP,      KC_1,    KC_2,      KC_3,    KC_0,      \
+  _______,    _______, _______, _______, _______, _______,  _______,     _______,     KC_HOME, KC_PGDOWN, KC_PGUP, KC_END     \
 ),  
 
 };
