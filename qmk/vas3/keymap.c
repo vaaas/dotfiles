2f3779c88902a5b3a10d2bac4f6116dc9ec088e0
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-10-04 11:45:01 +0300

    layouts

--- qmk/vas3/keymap.c
+++ qmk/vas3/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT_ortho_4x12( \
   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , \
-  _______, KC_MINS, KC_EQL , KC_TAB , KC_ENT , _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, \
+  _______, KC_MINS, KC_EQL , KC_TAB , KC_ENT , KC_KANA, KC_HENK, KC_LBRC, KC_RBRC, KC_VOLU, KC_VOLD, KC_MUTE, \
   _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F12 , \
   KC_APP , _______, _______, _______, _______, _______, _______, KC_BSPC, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 ),
