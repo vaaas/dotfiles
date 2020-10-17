fcef3d628ef4f4236b882ccea6177d39f68aabf6
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-10-17 14:28:53 +0300

    proper media keys orientation

--- qmk/jjvas/keymap.c
+++ qmk/jjvas/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , \
   _______, KC_MINS, KC_EQL , KC_TAB , KC_ENT , KC_P1  , KC_P2  , KC_LBRC, KC_RBRC, KC_P3  , KC_P4  , KC_P5  , \
   _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F12 , \
-  KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR, _______, KC_KANA, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+  KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR, _______, KC_KANA, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 ),
 
 };
