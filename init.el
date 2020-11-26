4e4025db6f206b5d31388d26a406076c877948c8
Vasileios Pasialiokis <pasivasi@outlook.com>
2020-11-26 10:24:25 +0200

    finally back to emacs

--- init.el
+++ init.el
@@ -1,10 +1,37 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 (electric-indent-mode -1)
+(ido-mode 1)
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4)
+(setq tab-stop-list '(4))
+
+(defun expand-or-tab()
+    (interactive)
+    (if (member (char-before) '(10 32))
+        (tab-to-tab-stop)
+        (call-interactively 'dabbrev-expand)))
+
+(defun line-below()
+    (interactive)
+    (end-of-line) (open-line 1) (next-line))
+
+(defun do-nothing() (interactive))
+
+(defun quick-find-file()
+    (interactive)
+    (find-file
+    (ido-completing-read "select file> "
+    (split-string
+    (shell-command-to-string "xzcat ~/filedb.xz") "\n"))))
 
 (define-minor-mode vi-mode
-  "Get your foos in the right places."
   :lighter " vi"
   :keymap (let ((map (make-sparse-keymap)))
     (define-key map (kbd "u") 'vi-mode)
@@ -12,11 +39,19 @@
     (define-key map (kbd "e") 'next-line)
     (define-key map (kbd "o") 'previous-line)
     (define-key map (kbd "a") 'right-char)
+    (define-key map (kbd "I") 'beginning-of-line)
+    (define-key map (kbd "E") 'forward-paragraph)
+    (define-key map (kbd "O") 'backward-paragraph)
+    (define-key map (kbd "A") 'end-of-line)
+    (define-key map (kbd "d d") 'kill-whole-line)
+    (define-key map (kbd "l") (lambda() (interactive) (line-below) (vi-mode -1)))
+    (define-key map (kbd "<escape>") 'do-nothing)
     map))
 
 (define-key global-map (kbd "C-s") 'save-buffer)
-(define-key global-map (kbd "C-i") 'dabbrev-expand)
+(define-key global-map (kbd "C-u") 'vi-mode)
 (define-key global-map (kbd "C-o") 'find-file)
+(define-key global-map (kbd "C-S-O") 'quick-find-file)
 (define-key global-map (kbd "C-w") 'backward-kill-word)
 (define-key global-map (kbd "C-k") 'kill-this-buffer)
 (define-key global-map (kbd "C-z") 'undo)
@@ -25,21 +60,40 @@
 (define-key global-map (kbd "C-,") 'previous-buffer)
 (define-key global-map (kbd "C-.") 'next-buffer)
 (define-key global-map (kbd "M-f") 'execute-extended-command)
+(define-key global-map (kbd "M-F") 'eval-expression)
 (define-key global-map (kbd "C-t") 'switch-to-buffer)
 (define-key global-map (kbd "C-x") 'kill-region)
 (define-key global-map (kbd "C-c") 'kill-ring-save)
 (define-key global-map (kbd "C-v") 'yank)
 (define-key global-map (kbd "<escape>") 'vi-mode)
+(define-key global-map (kbd "C-<tab>") 'ido-switch-buffer)
+(define-key global-map (kbd "C-f") 'isearch-forward)
+
+(define-key prog-mode-map (kbd "<tab>") 'expand-or-tab)
+
+(define-key minibuffer-local-map (kbd "<escape>") 'abort-recursive-edit)
+
+(define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
+(define-key isearch-mode-map (kbd "<escape>") 'isearch-abort)
+(define-key isearch-mode-map (kbd "<return>") 'isearch-complete)
+
+(add-hook 'prog-mode-hook (lambda() (vi-mode 1)))
+(add-hook 'minibuffer-setup-hook (lambda() (vi-mode -1)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages (quote (vue-mode php-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#fff3e0" :foreground "#303030" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "1ASC" :family "Liberation Mono")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#fff3e0" :foreground "#303030" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "1ASC" :family "Liberation Mono"))))
+ '(font-lock-function-name-face ((t (:foreground "#048"))))
+ '(font-lock-keyword-face ((t (:foreground "#a05"))))
+ '(font-lock-string-face ((t (:foreground "#084"))))
+ '(font-lock-type-face ((t (:foreground "#088"))))
+ '(mmm-default-submode-face ((t nil))))
