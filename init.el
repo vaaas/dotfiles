76f953e77f39a53733bb884b951ce48f0126e15e
Vasileios Pasialiokis <pasivasi@outlook.com>
2020-12-03 16:31:12 +0200

    merge init.el from work

--- init.el
+++ init.el
@@ -1,12 +1,4 @@
 ;; -*- lexical-binding: t; -*-
-(setq
-    gc-cons-threshold most-positive-fixnum
-    gc-cons-percentage 0.6)
-
-(add-hook 'emacs-startup-hook (lambda () (setq
-    gc-cons-threshold 16777216
-    gc-cons-percentage 0.1)))
-
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -20,46 +12,53 @@
 (abbrev-mode 1)
 
 (setq-default indent-tabs-mode nil
-    ;mode-line-format nil
-    line-spacing 0.3)
+	line-spacing 0.3
+	tab-width 4)
 (setq inhibit-splash-screen t
-    inhibit-startup-message t
-    tab-width 4
-    tab-stop-list '(4 8 12 16 20 24 28 32 36 40)
-    frame-resize-pixelwise t
-    vc-follow-symlinks t
-    make-backup-files nil)
+	inhibit-startup-message t
+	frame-resize-pixelwise t
+	vc-follow-symlinks t
+	make-backup-files nil)
 (setf (cdr (assq 'continuation fringe-indicator-alist)) '(nil nil))
 
 (put 'dired-find-alternate-file 'disabled nil)
 
-(defun expand-or-tab()
-    (interactive)
-    (if (member (char-before) '(10 32))
-        (tab-to-tab-stop)
-        (call-interactively 'dabbrev-expand)))
+(defun expand-or-tab() (interactive)
+	(if (member (char-before) '(10 32))
+		(if indent-tabs-mode (insert-char 9) (insert-char 32 tab-width))
+		(call-interactively 'dabbrev-expand)))
 
-(defun line-below()
-    (interactive)
-    (end-of-line) (open-line 1) (next-line))
+(defun line-below() (interactive)
+	(end-of-line) (open-line 1) (next-line))
 
-(defun newline-and-indent-relative() (interactive) (newline) (indent-relative))
+(defun newline-and-indent-relative() (interactive) (newline) (indent-relative t t))
 
 (defun do-nothing() (interactive))
 
-(defun quick-find-file()
-    (interactive)
-    (find-file
-    (ido-completing-read "select file> "
-    (split-string
-    (shell-command-to-string "xzcat ~/filedb.xz") "\n"))))
+(defun quick-find-file() (interactive)
+	(find-file
+	(ido-completing-read "select file> "
+	(split-string
+	(shell-command-to-string "xzcat ~/filedb.xz") "\n"))))
 
 (defun dired-here() (interactive) (dired default-directory))
 
 (defun backspace-or-unindent() (interactive)
-    (if (string= "    " (buffer-substring (point) (- (point) 4)))
-        (backward-delete-char 4)
-        (backward-delete-char 1)))
+	(if indent-tabs-mode
+		(backward-delete-char 1)
+		(if (string= (make-string tab-width ? ) (buffer-substring (point) (- (point) tab-width)))
+			(backward-delete-char 4)
+			(backward-delete-char 1))))
+
+(defun double-newline() (interactive)
+	(if (= 10 (char-before))
+		(newline)
+		(progn (newline) (newline))))
+
+(defun toggle-indent-tabs() (interactive)
+	(if indent-tabs-mode
+		(progn (setq indent-tabs-mode nil) (message "indent will use SPACES"))
+		(progn (setq indent-tabs-mode t) (message "indent will use TABS"))))
 
 (setq vi-mode-map (make-sparse-keymap))
 (define-key vi-mode-map (kbd "q") 'kmacro-start-macro)
@@ -74,9 +73,10 @@
 (define-key vi-mode-map (kbd "O") 'backward-paragraph)
 (define-key vi-mode-map (kbd "A") 'end-of-line)
 (define-key vi-mode-map (kbd "d d") 'kill-whole-line)
+(define-key vi-mode-map (kbd "D") 'kill-region)
 (define-key vi-mode-map (kbd "l") (lambda() (interactive) (line-below) (vi-mode -1)))
 (define-key vi-mode-map (kbd "<escape>") 'do-nothing)
-(define-key vi-mode-map (kbd "w") 'forward-whitespace)
+(define-key vi-mode-map (kbd "w") 'forward-to-word)
 (define-key vi-mode-map (kbd "b") 'backward-word)
 (define-key vi-mode-map (kbd "c w") (lambda() (interactive) (kill-word 1) (vi-mode -1)))
 (define-key vi-mode-map (kbd "c b") (lambda() (interactive) (backward-kill-word 1) (vi-mode -1)))
@@ -90,6 +90,7 @@
 (define-key vi-mode-map (kbd ".") 'repeat)
 (define-key vi-mode-map (kbd "\\ v p") 'variable-pitch-mode)
 (define-key vi-mode-map (kbd "\\ e a") 'edit-abbrevs)
+(define-key vi-mode-map (kbd "\\ i t") 'toggle-indent-tabs)
 (define-key vi-mode-map (kbd "j e") (lambda() (interactive) (next-line) (beginning-of-line-text) (delete-indentation)))
 (define-key vi-mode-map (kbd "v") 'set-mark-command)
 (define-key vi-mode-map (kbd "<backtab>") 'indent-rigidly-left-to-tab-stop)
@@ -103,11 +104,13 @@
 (define-key vi-mode-map (kbd ":") 'eval-expression)
 (define-key vi-mode-map (kbd ";") (lambda() (interactive) (end-of-line) (insert-char #x3B)))
 (define-key vi-mode-map (kbd "y") 'kill-ring-save)
+(define-key vi-mode-map (kbd "g g") 'beginning-of-buffer)
+(define-key vi-mode-map (kbd "G") 'end-of-buffer)
 
 (define-minor-mode vi-mode
-    "Ghetto vi mode"
-    :lighter " vi"
-    :keymap 'vi-mode-map)
+	"Ghetto vi mode"
+	:lighter " vi"
+	:keymap 'vi-mode-map)
 
 (define-key global-map (kbd "C-s") 'save-buffer)
 (define-key global-map (kbd "C-u") 'vi-mode)
@@ -151,20 +154,21 @@
 (add-hook 'minibuffer-setup-hook (lambda() (vi-mode -1)))
 (add-hook 'text-mode-hook (lambda() (abbrev-mode 1)))
 (add-hook 'eshell-mode-hook (lambda()
-    (vi-mode -1)
-    (define-key eshell-mode-map (kbd "M-`") 'kill-this-buffer)))
+	(vi-mode -1)
+	(define-key eshell-mode-map (kbd "M-`") 'kill-this-buffer)))
 (add-hook 'dired-mode-hook (lambda() (dired-hide-details-mode)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (with-eval-after-load 'markdown-mode
-    (setq markdown-mode-map (make-sparse-keymap)))
+	(setq markdown-mode-map (make-sparse-keymap))
+	(define-key markdown-mode-map (kbd "<return>") 'double-newline))
 
 (with-eval-after-load 'php-mode
-    (setq php-mode-map (make-sparse-keymap)))
+	(setq php-mode-map (make-sparse-keymap)))
 
 (with-eval-after-load 'dired
-    (define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
-    (define-key dired-mode-map (kbd "C-o") nil))
+	(define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
+	(define-key dired-mode-map (kbd "C-o") nil))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -178,7 +182,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#fff3e0" :foreground "#303030" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "1ASC" :family "Liberation Mono"))))
- '(font-lock-builtin-face ((t (:underline (:color foreground-color :style wave)))))
+ '(font-lock-builtin-face ((t (:underline (:color foreground-color)))))
  '(font-lock-constant-face ((t nil)))
  '(font-lock-function-name-face ((t (:foreground "#048"))))
  '(font-lock-keyword-face ((t (:weight semi-bold))))
