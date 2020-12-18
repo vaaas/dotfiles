16b60cd913b15a927feab78b58acf36e03caa6f6
Vasileios Pasialiokis <pasivasi@outlook.com>
2020-12-18 14:12:56 +0200

    emacs

--- init.el
+++ init.el
@@ -185,6 +185,7 @@
 (define-key global-map (kbd "C-<tab>") 'ido-switch-buffer)
 (define-key global-map (kbd "<C-return>") 'line-below)
 (define-key global-map (kbd "<f2>") 'dired-here)
+(define-key global-map (kbd "C-g") 'goto-line)
 
 (define-key prog-mode-map (kbd "<tab>") 'expand-or-tab)
 (define-key prog-mode-map (kbd "<backtab>") 'indent-rigidly-left-to-tab-stop)
@@ -245,7 +246,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode vue-mode php-mode))))
+ '(package-selected-packages (quote (markdown-mode php-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
