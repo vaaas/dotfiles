63f7aab8d2ee6ce1605019138d7172ec970ccfe5
Vasileios Pasialiokis <whiterocket@outlook.com>
2020-12-31 18:19:43 +0200

    oops

--- qmk/jjvas/keymap.c
+++ qmk/jjvas/keymap.c
@@ -29,7 +29,7 @@ KC_ESC , KC_DEL , KC_INS , KC_APP , MO(1)  , KC_SPC , KC_BSPC, KC_LSFT, KC_LEFT,
 ),
 [_RAISE] = LAYOUT_ortho_4x12( \
 _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , \
-_______, KC_RGUI, KC_RCTL, KC_RALT  KC_RSFT, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_P1  , KC_P2  , KC_P3  , \
+_______, KC_RGUI, KC_RCTL, KC_RALT, KC_RSFT, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_P1  , KC_P2  , KC_P3  , \
 _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F12 , \
 KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_ENT , KC_TAB , _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 ),
--- qmk/xdvas/keymap.c
+++ qmk/xdvas/keymap.c
@@ -23,16 +23,16 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_5x15(
-KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSLS, KC_PAUS, KC_CAPS, KC_PSCR,
-KC_LGUI, KC_K   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_MINS, KC_F10 , KC_F11 , KC_F12 ,
+KC_NUHS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NUBS, KC_PAUS, KC_CAPS, KC_PSCR,
+KC_LGUI, KC_K   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_GRV , KC_F10 , KC_F11 , KC_F12 ,
 KC_LCTL, KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_O   , KC_A   , KC_QUOT, KC_F7  , KC_F8  , KC_F9  ,
-KC_LALT, KC_Z   , KC_F   , KC_G   , KC_L   , KC_X   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_EQL , KC_F4  , KC_F5  , KC_F6  ,
+KC_LALT, KC_Z   , KC_F   , KC_G   , KC_L   , KC_X   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS, KC_F4  , KC_F5  , KC_F6  ,
 KC_ESC , KC_DEL , KC_INS , KC_APP , MO(1) ,  KC_SPC,  KC_BSPC, KC_LSFT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_F1  , KC_F2  , KC_F3
 ),
 [_RAISE] = LAYOUT_ortho_5x15(
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, KC_RGUI, KC_RCTL, KC_RALT, KC_RSFT, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_NUHS, KC_NUBS, KC_RO  , _______, _______, _______,
+_______, KC_RGUI, KC_RCTL, KC_RALT, KC_RSFT, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_P1  , KC_P2  , KC_P3  , _______, _______, _______,
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_ENT , KC_TAB , _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______, _______
 )
