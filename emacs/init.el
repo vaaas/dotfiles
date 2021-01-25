c6ce8c0c4911fe774b380de194901de908076123
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-01-25 15:40:01 +0200

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -336,7 +336,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode php-mode)))
+ '(package-selected-packages (quote (restclient markdown-mode php-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -355,8 +355,8 @@
  '(font-lock-type-face ((t nil)))
  '(font-lock-variable-name-face ((t nil)))
  '(fringe ((t nil)))
- '(isearch ((t (:background "#29ADFF" :foreground "#FFF1E8"))))
  '(highlight ((t (:background "#1D2B53" :foreground "#FFF1E8"))))
+ '(isearch ((t (:background "#29ADFF" :foreground "#FFF1E8"))))
  '(lazy-highlight ((t (:inherit highlight))))
  '(minibuffer-prompt ((t (:foreground "#7E2553"))))
  '(mmm-default-submode-face ((t nil)))
