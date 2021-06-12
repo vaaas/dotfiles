f348771e6e2ee99eb4c208945931f9434c1cd809
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-06-12 14:48:57 +0300

    add home row mods

--- qmk/jjvas/keymap.c
+++ qmk/jjvas/keymap.c
@@ -21,19 +21,29 @@ enum layers {
 };
 
 #define LT_APP LT(1, KC_APP)
-#define ALT_ESC LALT_T(KC_ESC)
+#define LT_ENT LT(1, KC_ENT)
+#define CTL_KN LCTL_T(KC_N)
+#define CTL_KI RCTL_T(KC_I)
+#define SFT_KT LSFT_T(KC_T)
+#define SFT_KE RSFT_T(KC_E)
+#define ALT_KS LALT_T(KC_S)
+#define ALT_KO LALT_T(KC_O)
+#define GUI_KR LGUI_T(KC_R)
+#define GUI_KA RGUI_T(KC_A)
+#define ALT_SPC RALT_T(KC_SPC)
+#define ALT_TAB RALT_T(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_4x12( \
-KC_MINS, KC_X   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_GRV , \
-KC_LCTL, KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_O   , KC_A   , KC_QUOT, \
+KC_NUHS, KC_X   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_GRV , \
+KC_MINS, GUI_KR , ALT_KS , SFT_KT , CTL_KN , KC_H   , KC_Y   , CTL_KI , SFT_KE , ALT_KO , GUI_KA , KC_QUOT, \
 KC_EQL , KC_Z   , KC_F   , KC_G   , KC_L   , KC_K   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS, \
-KC_SLCK, KC_NLCK, KC_CAPS, KC_LGUI, LT_APP , KC_SPC , ALT_ESC, KC_LSFT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
+KC_HOME, KC_PGDN, KC_PGUP, KC_END , LT_APP , ALT_SPC, ALT_TAB, LT_ENT , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT  \
 ),
 [_RAISE] = LAYOUT_ortho_4x12( \
-KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , \
-_______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , KC_KP_4, KC_KP_5, KC_LBRC, KC_RBRC, KC_KP_1, KC_KP_2, KC_KP_3, \
-KC_KP_6, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, \
-KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_INS , _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+KC_F11 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12 , \
+KC_LBRC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_RBRC, \
+KC_CAPS, KC_P1  , KC_P2  , KC_P3  , KC_P4  , KC_P5  , KC_P6  , KC_P7  , KC_P8  , KC_P9  , KC_P0  , KC_NLCK, \
+KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_ESC , KC_INS , KC_DEL , KC_BSPC, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT  \
 ),
 };
