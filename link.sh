ff820dc2711502a11d3b19026ccb6035d89dfa87
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-07-01 10:44:00 +0300

    reorganisation

--- link.sh
+++ link.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 IFS='
 '
-while read line
-do
-	src=$(echo $line | cut -f 1 -d '	')
-	dst=$(echo $line | cut -f 2 -d '	')
-	dst=${dst/#\~/$HOME}
-	ln -s -v -- $(realpath $src) $dst
-done < files.lst
+alias lns='ln -s -v --'
+
+# while read line
+# do
+# 	src=$(echo $line | cut -f 1 -d '	')
+# 	dst=$(echo $line | cut -f 2 -d '	')
+# 	dst=${dst/#\~/$HOME}
+# 	ln -s -v -- $(realpath $src) $dst
+# done < files.lst
+
+for x in home/*
+do lns $(realpath $x) ~/.$(basename $x)
+done
+
+mkdir -p ~/Binaries
+for x in bin/*
+do lns $(realpath $x) ~/Binaries/$(basename $x)
+done
+
+mkdir -p ~/.config/emacs
+for x in emacs/*
+do lns $(realpath $x) ~/.config/emacs/$(basename $x)
+done
