5352feede23892bd2be3db3f9a679561f95d4da5
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-07-01 10:59:26 +0300

    finish the new linker

--- link.sh
+++ link.sh
@@ -3,24 +3,31 @@ IFS='
 '
 alias lns='ln -s -v --'
 
-# while read line
-# do
-# 	src=$(echo $line | cut -f 1 -d '	')
-# 	dst=$(echo $line | cut -f 2 -d '	')
-# 	dst=${dst/#\~/$HOME}
-# 	ln -s -v -- $(realpath $src) $dst
-# done < files.lst
-
+# ~ files
 for x in home/*
 do lns $(realpath $x) ~/.$(basename $x)
 done
 
+# binaries
 mkdir -p ~/Binaries
 for x in bin/*
 do lns $(realpath $x) ~/Binaries/$(basename $x)
 done
 
-mkdir -p ~/.config/emacs
-for x in emacs/*
-do lns $(realpath $x) ~/.config/emacs/$(basename $x)
+# ~/.config
+for x in $(find config -type -d | tail -n +2)
+do mkdir -p ~/.$x
+done
+
+for x in $(find config -type f)
+do lns $(realpath $x) ~/.$x
+done
+
+# ~/.local/share
+for x in $(find share -type -d | tail -n +2)
+do mkdir -p ~/.local/$x
+done
+
+for x in $(find config -type f)
+do lns $(realpath $x) ~/.local/$x
 done
