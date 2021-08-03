2d715ce5b83f65d9a4731058727bd99a60a8d6b4
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-08-03 03:52:50 +0300

    layout updates

--- qmk/jjvas/keymap.c
+++ qmk/jjvas/keymap.c
@@ -31,6 +31,6 @@ KC_HOME, KC_PGDN, KC_PGUP, KC_END , MO(1)  , KC_SPC , KC_RALT, KC_LSFT, KC_LEFT,
 _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , \
 _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV , \
 _______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , _______, _______, KC_LBRC, KC_RBRC, _______, KC_BSLS, KC_F12 , \
-KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_ESC , KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT  \
+KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT  \
 ),
 };
--- qmk/xdvas/keymap.c
+++ qmk/xdvas/keymap.c
@@ -23,7 +23,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_ortho_5x15(
-KC_NUHS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NUBS, KC_F13 , KC_F14 , KC_F15 , \
+KC_LBRC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_RBRC, KC_BSLS, KC_GRV , KC_JYEN, \
 KC_LGUI, KC_X   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_MINS, KC_F10 , KC_F11 , KC_F12 , \
 KC_LCTL, KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_O   , KC_A   , KC_QUOT, KC_F7  , KC_F8  , KC_F9  , \
 KC_LALT, KC_Z   , KC_F   , KC_G   , KC_L   , KC_K   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_EQL , KC_F4  , KC_F5  , KC_F6  , \
@@ -32,8 +32,8 @@ KC_HOME, KC_PGDN, KC_PGUP, KC_END , MO(1)  , KC_SPC , KC_RALT, KC_LSFT, KC_LEFT,
 [_RAISE] = LAYOUT_ortho_5x15(
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-_______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_GRV , _______, _______, _______, \
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______, _______, _______, _______, \
-KC_INS , KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_DEL , KC_BSPC, _______, KC_PSCR, KC_CAPS, KC_NLCK, KC_SLCK, _______, _______, _______  \
+_______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT, _______, _______, _______  \
 )
 };
