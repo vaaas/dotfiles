fe5558c33efda26502545145bc0246538a4d6d7e
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-09-28 17:06:44 +0300

    neocities

--- config/emacs/neocities.el
+++ config/emacs/neocities.el
@@ -20,9 +20,9 @@
 		(concat "<!DOCTYPE html>" (seml-to-html
 		(nc-inline
 		(nc-frontpage conf
-		(map (lambda(x) (nc-render-item conf x))
-		(seq-filter (lambda(x) (str-plist-get "skip" (nth 1 x)))
-		posts)))))))
+		(map 'nc-render-item
+		(seq-filter (lambda(x) (str-plist-get "skip" (nth 1 x))))
+		posts))))))
 	(with-temp-file (concat blog-directory "/render/rss.xml")
 		(concat "<?xml version='1.0' encoding='UTF-8'?>" (seml-to-html
 		(nc-rss conf
@@ -35,3 +35,49 @@
 	(dolist (x (seq-filter (lambda(x) (str-plist-get "filename" (nth 1 x))) posts))
 		(with-temp-file (concat blog-directory "/render/" (str-plist-get "filename" x))
 			(concat "<!DOCTYPE html>" (seml-to-html (nc-inline (nc-page x))))))))
+
+(defun nc-rencer-item (x)
+	(list "article"
+		(list ("id" (nc-guid (string-to-number (str-plist-get "timestamp" (nth 1 x)))))
+			("t" (str-plist-get "tag" (nth 1 x))))
+		(nc-description x)))
+
+(defun nc-description (x)
+	(let ((xs ()) (h1 nil) (p nil))
+	(push (list "time" nil (ymd (int (str-plist-get "timestamp" (nth 1 x))))) x)
+	(if (str-plist-get "filename" x)
+	(progn
+		(setq h1 (query-selector (seml-elem= "h1") x))
+		(when (not h1) (throw 'bad-post "No h1 found"))
+		(setq p (query-selector (seml-elem= "p") p))
+		(when (not p) (throw 'bad-post "No p found"))
+		(push (list "h1" nil (list
+			(list "a"
+				(list "href" (concat "/" (str-plist-get "filename" (nth 1 x))))
+				(nth 2 h1))))
+			xs)
+		(push (nth 2 p)))
+	(dolist (y (nth 2 x)) (push y xs)))
+	(nreverse xs)))
+
+(defun seml-elem= (s) (lambda (x) (string= s (car x))))
+
+(defun query-selector (f node)
+	(let ((head node) (found nil) (children (nth 2 node)))
+	(while (and head (not found))
+		(when (funcall f (car head)) (setq found (car head)))
+		(while (and children (not found))
+			(setq found (query-selector f (car children)))
+			(setq children (cdr children)))
+		(setq head (car node)))
+	found))
+
+(defun query-selector-all (f node)
+	(let ((head node) (found ()) (children (nth 2 node)))
+	(while head
+		(when (funcall f (car head)) (push (car head) found))
+		(while children
+			(setcdr (last found) (query-selector f (car children)))
+			(setq children (cdr children)))
+		(setq head (car node)))
+	found))
