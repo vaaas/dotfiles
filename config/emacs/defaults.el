9da829c156d58f104085884b2856d9de215c85fb
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-10-09 11:22:54 +0300

    saner defaults

--- config/emacs/defaults.el
+++ config/emacs/defaults.el
@@ -7,9 +7,9 @@
 (prefer-coding-system 'utf-8-unix)
 
 (setq-default
-	indent-tabs-mode nil
+	indent-tabs-mode (when (string= (system-name) "cleome") t)
 	line-spacing 0.3
-	tab-width 4
+	tab-width (if (string= (system-name) "cleome") 3 4)
 	mode-line-format nil
 	version-control "never"
 	create-lockfiles nil
@@ -52,25 +52,30 @@
 (add-to-list 'auto-mode-alist '("\\.blade.php\\'" . js-mode))
 
 (custom-set-variables
+	;; custom-set-variables was added by Custom.
+	;; If you edit it by hand, you could mess it up, so be careful.
+	;; Your init file should contain only one such instance.
+	;; If there is more than one, they won't work right.
 	'(package-selected-packages '(php-mode)))
 (custom-set-faces
-	'(default (list (list t (list :inherit nil :stipple nil :background "#ffeedd" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant 'normal :weight 'normal :height 140 :width 'normal :family "JuliaMono"))))
+	;; custom-set-faces was added by Custom.
+	;; If you edit it by hand, you could mess it up, so be careful.
+	;; Your init file should contain only one such instance.
+	;; If there is more than one, they won't work right.
+	'(default ((t (:inherit nil :extend nil :stipple nil :background "#ffeedd" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "1ASC" :family "Liberation Mono"))))
 	'(cursor ((t (:background "#ff0055"))))
 	'(eshell-ls-directory ((t (:foreground "#2255aa" :weight bold))))
 	'(eshell-ls-executable ((t (:foreground "#008844" :weight bold))))
 	'(eshell-prompt ((t (:foreground "#ff0055" :weight bold))))
 	'(font-lock-builtin-face ((t (:underline (:color foreground-color)))))
 	'(font-lock-comment-face ((t (:foreground "#aa4422"))))
-	'(font-lock-type-face ((t (:foreground "#d33682"))))
 	'(font-lock-constant-face ((t (:inherit font-lock-type-face))))
 	'(font-lock-function-name-face ((t (:foreground "#2255aa" :weight bold))))
 	'(font-lock-keyword-face ((t (:weight semi-bold))))
 	'(font-lock-string-face ((t (:foreground "#008844"))))
-	'(font-lock-type-face ((t :foreground "#884488")))
+	'(font-lock-type-face ((t (:foreground "#d33682"))))
 	'(font-lock-variable-name-face ((t nil)))
 	'(fringe ((t nil)))
-	'(cursor ((t (:background "#ff0055"))))
-	'(region ((t (:background "#ffffff"))))
 	'(highlight ((t (:background "#ccccff"))))
 	'(isearch ((t (:background "#2255aa" :foreground "#ffeedd"))))
 	'(lazy-highlight ((t (:inherit highlight))))
@@ -81,4 +86,5 @@
 	'(php-$this ((t (:slant oblique))))
 	'(php-$this-sigil ((t (:inherit php-$this))))
 	'(php-function-call ((t (:inherit font-lock-function-name-face))))
+	'(region ((t (:background "#ffffff"))))
 	'(show-paren-match ((t (:inherit highlight)))))
