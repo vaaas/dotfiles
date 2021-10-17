caba95f0202b77f33c8572e24e441900f158f8cf
Vasileios Pasialiokis <whiterocket@outlook.com>
2021-10-17 12:33:54 +0300

    init.el

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -8,6 +8,5 @@
 (require 'xml)
 
 ; I have split the init.el file into several more dedicated files. Dynamically load these files
-(seq-each (lambda (x) (load-file (concat user-emacs-directory "/" x)))
-(seq-filter (lambda (x) (and (string-suffix-p ".el" x) (not (string= "init.el" x))))
-(directory-files user-emacs-directory)))
+(dolist (x (directory-files (concat user-emacs-directory "/elisp" )))
+	(load-file (concat user-emacs-directory "/elisp/" x)))
