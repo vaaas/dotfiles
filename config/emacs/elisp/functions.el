aa5d8720e93ce6bbc17f695ced60d2468e1f56ca
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-11-26 07:04:31 +0000

    move macros to dedicated macros file

--- config/emacs/elisp/functions.el
+++ config/emacs/elisp/functions.el
@@ -1,30 +1,6 @@
 ; -*- lexical-binding: t -*-
 ; various utility functions
 
-(defmacro L (x &rest body)
-	"lambda shorthand for only one argument."
-	`(lambda (,x) ,@body))
-
-(defmacro C (f &rest body)
-	"call F with arguments BODY in reverse."
-	(append (list f) (nreverse body)))
-
-(defmacro -> (&rest body)
-	"Pipeline macro. Pass the first element of body to the second, replacing the placeholder `$'.
-Thus, (+ 1 (+ 2 x)) becomes (-> x (+ 2 $) (+ 1 $))"
-	(let ((result (pop body)))
-	(dolist (form body result)
-		(setq result (mapcar (L x (if (eq '$ x) result x)) form)))))
-
-(defmacro => (&rest body)
-	"Like ->, excepts returns a lambda, that when evaluated, will run its argument through the pipeline.
-Thus, (mapcar (lambda (x) (+ 1 (+ 2 x))) xs) becomes (mapcar (=> (+ 2 $) (+ 1 $)) xs)"
-	`(L x (-> x ,@body)))
-
-(defmacro lastcar (x)
-	"Returns the last car."
-	`(car (last ,x)))
-
 (defun outside (xs)
 	"Curried function. Returns lambda which checks whether X is a member of XS"
 	(L x (not (member x xs))))
@@ -95,16 +71,6 @@ Thus, (mapcar (lambda (x) (+ 1 (+ 2 x))) xs) becomes (mapcar (=> (+ 2 $) (+ 1 $)
 	(let ((r (cons (car xs) nil)))
 	(nreverse (dolist (x (cdr xs) r) (push s r) (push x r)))))
 
-(defmacro with-temp-dir (temp-dir &rest body)
-	"Change directory to TEMP-DIR. Execute BODY. Then return to the previous directory."
-	`(let ((old default-directory))
-	(cd ,temp-dir)
-	(unwind-protect (progn ,@body) (cd old))))
-
-(defmacro push-all (from to)
-	"`push' all elements of FROM onto TO"
-	`(dolist (x ,from ,to) (setq ,to (cons x ,to))))
-
 (defun read-elisp-file (file)
 	"`read' the elisp file FILE"
 	(with-current-buffer (find-file-noselect file)
@@ -136,7 +102,7 @@ BINDINGS should be an alist where car is a `kbd' string and cdr is a function."
 
 (defun sha1-ext (x)
 	"sha1 a file X using external sha1 command"
-	(->(list "sha1sum" "--" x)
+	(-> (list "sha1sum" "--" x)
 	(string-join $ " ")
 	(shell-command-to-string $)
 	(split-string $ " ")
@@ -146,38 +112,6 @@ BINDINGS should be an alist where car is a `kbd' string and cdr is a function."
 	"Return the set difference of AS - BS. (items of AS that are not on BS)"
 	(seq-filter (outside bs) as))
 
-(defmacro with-contents-function (buffer setup &rest rest)
-	"Switch to buffer BUFFER, optionally running SETUP. Set after-save and before-save hooks to nill, then set write-contents-functions to a single function, as defined in REST.
-
-This is useful for creating temporary non-file buffers and waiting for the user to save the buffer to continue execution."
-	`(progn
-		(switch-to-buffer ,buffer)
-		(erase-buffer)
-		,setup
-		(setq-local
-			after-save-hook nil
-			before-save-hook nil
-			write-contents-functions (list (lambda() ,@rest (kill-buffer ,buffer))))
-		(concat "Editing virtual " ,buffer ". File will not be saved.")))
-
-(defmacro edit-buffer-region (buffer start end &rest setup)
-	"Create a buffer BUFFER, cloning the region from START to END, optionally running SETUP. Upon save, replaces the original buffer's region with the contents of BUFFER."
-	`(let ((prev (current-buffer)))
-	(with-contents-function ,buffer
-		(progn
-			(insert-buffer-substring-no-properties prev ,start ,end)
-			(beginning-of-buffer)
-			,@setup)
-		(switch-to-buffer prev)
-		(delete-region ,start ,end)
-		(goto-char ,start)
-		(insert-buffer-substring-no-properties ,buffer)
-		(goto-char ,start))))
-
-(defmacro ignore-errors (&rest body)
-	"Execute BODY, returning nil on errors."
-	`(condition-case nil (progn ,@body) (error nil)))
-
 (defun string-head (n x)
 	"Get the first N characters of string X. If X is shorter than N, get the entire X."
 	(substring x 0 (min n (length x))))
@@ -185,7 +119,3 @@ This is useful for creating temporary non-file buffers and waiting for the user 
 (defun spread-last (x)
 	"spread the last element of X onto X. Useful for XML"
 	(append (butlast x) (lastcar x)))
-
-(defmacro whenl (x v &rest body)
-	"convenient composition of let and when"
-	`(let ((,x ,v)) (when ,x ,@body)))
---
+++ config/emacs/elisp/macros.el
@@ -0,0 +1,72 @@
+; -*- lexical-binding: t -*-
+; various utility macros
+
+(defmacro L (x &rest body)
+	"lambda shorthand for only one argument."
+	`(lambda (,x) ,@body))
+
+(defmacro C (f &rest body)
+	"call F with arguments BODY in reverse."
+	(append (list f) (nreverse body)))
+
+(defmacro -> (&rest body)
+	"Pipeline macro. Pass the first element of body to the second, replacing the placeholder `$'.
+Thus, (+ 1 (+ 2 x)) becomes (-> x (+ 2 $) (+ 1 $))"
+	(let ((result (pop body)))
+	(dolist (form body result)
+		(setq result (mapcar (L x (if (eq '$ x) result x)) form)))))
+
+(defmacro => (&rest body)
+	"Like ->, excepts returns a lambda, that when evaluated, will run its argument through the pipeline.
+Thus, (mapcar (lambda (x) (+ 1 (+ 2 x))) xs) becomes (mapcar (=> (+ 2 $) (+ 1 $)) xs)"
+	`(L x (-> x ,@body)))
+
+(defmacro lastcar (x)
+	"Returns the last car."
+	`(car (last ,x)))
+
+(defmacro whenl (x v &rest body)
+	"convenient composition of let and when"
+	`(let ((,x ,v)) (when ,x ,@body)))
+
+(defmacro ignore-errors (&rest body)
+	"Execute BODY, returning nil on errors."
+	`(condition-case nil (progn ,@body) (error nil)))
+
+(defmacro with-temp-dir (temp-dir &rest body)
+	"Change directory to TEMP-DIR. Execute BODY. Then return to the previous directory."
+	`(let ((old default-directory))
+	(cd ,temp-dir)
+	(unwind-protect (progn ,@body) (cd old))))
+
+(defmacro push-all (from to)
+	"`push' all elements of FROM onto TO"
+	`(dolist (x ,from ,to) (setq ,to (cons x ,to))))
+
+(defmacro with-contents-function (buffer setup &rest rest)
+	"Switch to buffer BUFFER, optionally running SETUP. Set after-save and before-save hooks to nil, then set write-contents-functions to a single function, as defined in REST.
+
+This is useful for creating temporary non-file buffers and waiting for the user to save the buffer to continue execution."
+	`(progn
+		(switch-to-buffer ,buffer)
+		(erase-buffer)
+		,setup
+		(setq-local
+			after-save-hook nil
+			before-save-hook nil
+			write-contents-functions (list (lambda nil ,@rest (kill-buffer ,buffer))))
+		(concat "Editing virtual " ,buffer ". File will not be saved.")))
+
+(defmacro edit-buffer-region (buffer start end &rest setup)
+	"Create a buffer BUFFER, cloning the region from START to END, optionally running SETUP. Upon save, replaces the original buffer's region with the contents of BUFFER."
+	`(let ((prev (current-buffer)))
+	(with-contents-function ,buffer
+		(progn
+			(insert-buffer-substring-no-properties prev ,start ,end)
+			(beginning-of-buffer)
+			,@setup)
+		(switch-to-buffer prev)
+		(delete-region ,start ,end)
+		(goto-char ,start)
+		(insert-buffer-substring-no-properties ,buffer)
+		(goto-char ,start))))
