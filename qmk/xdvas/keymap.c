941e072266aa8c15b8f15a2b3f9e1066823eef18
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-12-13 15:15:19 +0200

    update xdvas

--- qmk/xdvas/keymap.c
+++ qmk/xdvas/keymap.c
@@ -31,9 +31,9 @@ KC_HOME, KC_PGDN, KC_PGUP, KC_END , MO(1)  , KC_SPC , KC_RALT, KC_LSFT, KC_LEFT,
 ),
 [_RAISE] = LAYOUT_ortho_5x15(
 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , _______, _______, _______, \
+KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , _______, _______, _______, \
 _______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , KC_JYEN, KC_BSLS, KC_LBRC, KC_RBRC, KC_NUHS, KC_NUBS, KC_GRV , _______, _______, _______, \
-_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_F12 , _______, _______, _______, \
+_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_KANA, _______, _______, _______, \
 KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT, _______, _______, _______  \
 )
 };
