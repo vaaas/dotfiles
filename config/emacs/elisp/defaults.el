24e7a90a1e7a7291bb783725085dd1445defcf85
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-01-06 11:02:07 +0200

    variable pitch

--- config/emacs/elisp/defaults.el
+++ config/emacs/elisp/defaults.el
@@ -127,4 +127,4 @@
 	'(region ((t (:background "#fff"))))
 	'(show-paren-match ((t (:inherit highlight))))
 	'(markdown-code-face ((t (:inherit default :foreground "#d38"))))
-	'(variable-pitch ((t (:height 160 :family "Georgia")))))
+	'(variable-pitch ((t (:height 160 :family "Sans Serif")))))
