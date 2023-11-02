addf8363b04b50ca2ccf9031a48f6a6a08596645
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-11-02 19:33:24 +0200

    lint

--- qmk/xdvas/keymap.c
+++ qmk/xdvas/keymap.c
@@ -22,18 +22,19 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT_ortho_5x15(
-KC_LBRC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_RBRC, KC_F13 , KC_F14 , KC_F15 , \
-KC_LGUI, KC_X   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_MINS, KC_F10 , KC_F11 , KC_F12 , \
-KC_LCTL, KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_O   , KC_A   , KC_QUOT, KC_F7  , KC_F8  , KC_F9  , \
-KC_LALT, KC_Z   , KC_F   , KC_G   , KC_L   , KC_K   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_EQL , KC_F4  , KC_F5  , KC_F6  , \
-KC_HOME, KC_PGDN, KC_PGUP, KC_END , MO(1)  , KC_SPC , KC_RALT, KC_LSFT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_F1  , KC_F2  , KC_F3    \
-),
-[_RAISE] = LAYOUT_ortho_5x15(
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , _______, _______, _______, \
-_______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , KC_JYEN, KC_BSLS, KC_LBRC, KC_RBRC, KC_NUHS, KC_NUBS, KC_GRV , _______, _______, _______, \
-_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_KANA, _______, _______, _______, \
-KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT, _______, _______, _______  \
-)
+	[_BASE] = LAYOUT_ortho_5x15(
+		KC_LBRC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_RBRC, KC_F13 , KC_F14 , KC_F15 , \
+		KC_LGUI, KC_X   , KC_C   , KC_D   , KC_M   , KC_J   , KC_Q   , KC_W   , KC_U   , KC_V   , KC_SCLN, KC_MINS, KC_F10 , KC_F11 , KC_F12 , \
+		KC_LCTL, KC_R   , KC_S   , KC_T   , KC_N   , KC_H   , KC_Y   , KC_I   , KC_E   , KC_O   , KC_A   , KC_QUOT, KC_F7  , KC_F8  , KC_F9  , \
+		KC_LALT, KC_Z   , KC_F   , KC_G   , KC_L   , KC_K   , KC_B   , KC_P   , KC_COMM, KC_DOT , KC_SLSH, KC_EQL , KC_F4  , KC_F5  , KC_F6  , \
+		KC_HOME, KC_PGDN, KC_PGUP, KC_END , MO(1)  , KC_SPC , KC_RALT, KC_LSFT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_F1  , KC_F2  , KC_F3    \
+	),
+
+	[_RAISE] = LAYOUT_ortho_5x15(
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+		KC_F12 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , _______, _______, _______, \
+		_______, KC_DEL , KC_BSPC, KC_TAB , KC_ENT , KC_JYEN, KC_BSLS, KC_LBRC, KC_RBRC, KC_NUHS, KC_NUBS, KC_GRV , _______, _______, _______, \
+		_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_KANA, _______, _______, _______, \
+		KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, _______, KC_INS , KC_ESC , _______, KC_MPRV, KC_BRID, KC_BRIU, KC_MNXT, _______, _______, _______  \
+	)
 };
