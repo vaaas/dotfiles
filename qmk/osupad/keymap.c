8724b6eb6343a154e5c1a86caa159095eb4cf82a
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-12-13 11:44:03 +0200

    move qmk files into qmk-config

--- qmk/osupad/keymap.c
+++
@@ -1,9 +0,0 @@
-#include QMK_KEYBOARD_H
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [0] = LAYOUT_ortho_4x4(
-      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
-      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
-   ),
-};
